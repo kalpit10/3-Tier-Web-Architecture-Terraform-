365f8e30956b0bbc4a35e2d9d0de80cc3c6d52b9
kalpit10 <kalpit.swami@gmail.com>
2025-08-16 19:42:22 -0400

    Added new key file

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -153,6 +153,12 @@ resource "tls_private_key" "ssh_key" {
 }
 
 resource "aws_key_pair" "generated_key" {
-  key_name   = "dev-key"
+  key_name   = "new-dev-key"
   public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
+resource "local_file" "private_key_file" {
+  content         = tls_private_key.ssh_key.private_key_pem
+  filename        = "${path.module}/new-dev-key.pem"
+  file_permission = "0600"
 }
